f3bc030e1540afe0e4ca5f16e2c8a534232d0025
Hell-Tractor <1104840448@qq.com>
2022-06-26 14:12:26 +0800

    Add patrick destroy effect

--- Assets/Scripts/Managers/BulletManager.cs
+++ Assets/Scripts/Managers/BulletManager.cs
@@ -10,15 +10,17 @@ public class BulletManager : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             collision.gameObject.GetComponent<PlayerController>().Kill(null);
-            this.Destory();
+            this.Destroy();
         }
         if (collision.gameObject.tag == "Ground") {
-            this.Destory();
+            this.Destroy();
         }
     }
 
-    public void Destory() {
-        _animator?.SetTrigger("OnDestory");
-        Destroy(this.gameObject);
+    public void Destroy() {
+        _animator?.SetTrigger("OnDestroy");
+        Destroy(this.GetComponent<Collider2D>());
+        Destroy(this.GetComponent<Rigidbody2D>());
+        Destroy(this.gameObject, 0.67f);
     }
 }
